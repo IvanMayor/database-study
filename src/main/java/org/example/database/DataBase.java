3bc0a426ae1d60bcf0beef1f1d01d41fa67ce888
Ivan <maramorosh2@gmail.com>
2026-03-04 21:22:50 -0500

    Just playing around with db to learn

--- src/main/java/org/example/database/DataBase.java
+++ src/main/java/org/example/database/DataBase.java
@@ -1,9 +1,6 @@
 package org.example.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.sql.*;
 
 
 public class DataBase {
@@ -17,11 +14,38 @@ public class DataBase {
     // We don't need to close connection because try with resources (so its closes automatically)
     public static void getConnection() {
 
-//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        String tableSql = "CREATE TABLE IF NOT EXISTS planes"
+//                + "(plane_id int PRIMARY KEY AUTO_INCREMENT, brand varchar(100),"
+//                + "fuel_per_hr DECIMAL(8,2), in_available BOOLEAN NOT NULL DEFAULT TRUE)"; -------Created Table Plane-----------
 
+//        String insertSql = "ALTER TABLE planes RENAME COLUMN in_available TO is_available"; ----Renamed field in_available to is_avail...----------
+//        String insertSql = "INSERT INTO planes(brand, fuel_per_hr, is_available)"
+//                + " VALUES('Boeing 747-400', 11000.00, TRUE)";                    -----------
+        String selectSql = "SELECT * FROM planes";
         try (Connection con = DriverManager
                 .getConnection(URL, USERNAME, PASS)) {
-            con.isValid(1000);
+
+            try (Statement stmt = con.createStatement()) {
+//                stmt.execute(insertSql);  ------Can execute both SELECT and INSERT
+//                stmt.executeUpdate(insertSql); -----------Can Insert something only
+                String updatePlaneSql = "UPDATE planes SET is_available=? WHERE plane_id=?";
+                try (PreparedStatement pstmt = con.prepareStatement(updatePlaneSql)) {
+                    pstmt.setBoolean(1, false);
+                    pstmt.setInt(2, 1);
+                    int rowsAffected = pstmt.executeUpdate();
+                    System.out.println(rowsAffected);
+
+                    try (ResultSet resultSet = stmt.executeQuery(selectSql)) {
+                        resultSet.next();
+                        System.out.println(resultSet.getInt("plane_id"));
+                        System.out.println(resultSet.getString("brand"));
+                        System.out.println(resultSet.getBigDecimal("fuel_per_hr"));
+                        System.out.println(resultSet.getBoolean("is_available"));
+
+                    }
+                }
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/org/example/database/PlanesService.java
@@ -0,0 +1,9 @@
+package org.example.database;
+
+import org.example.database.DataBase;
+
+public class PlanesService extends DataBase {
+    public void createPlane(String[] args) {
+
+    }
+}
